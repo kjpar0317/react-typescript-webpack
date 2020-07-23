f99a487f9148d4b9712131bb27278b772df11bec
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-07-23 18:01:50 +0900

    Update index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,30 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { App } from "./App";
+import { Helmet } from 'react-helmet';
+import { TITLE } from '@constants/index';
+import './index.scss';
+
+import createStore from './store';
+
+const store = createStore();
 
 ReactDOM.render(
-  <App userName="Beveloper" lang="TypeScript" />,
+    <>
+        <Helmet titleTemplate={`%s · ${TITLE}`} defaultTitle={TITLE}>
+        <meta
+            name="description"
+            content="Innogrid Container Management Platform - Shift Enter Cloudit"
+        />
+        </Helmet>
+        <BrowserRouter>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </BrowserRouter>
+    </>
+  ,
   document.getElementById("output")
 );
