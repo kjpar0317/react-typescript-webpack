dee95efc6425fd4a49be387f7a92c45c5e658622
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-07-23 17:56:42 +0900

    텍스트로 수정

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,23 +1,91 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ForkTsCheckerPlugin = require('fork-ts-checker-webpack-plugin');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+const env = require("dotenv");
+
+env.config();
+
+const { REACT_APP_BASE_URI } = process.env;
 
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: ["@babel/polyfill", "./src/index.tsx"],
   target: "web",
   mode: "development",
   output: {
-    path: path.resolve(__dirname, "public/dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'public/dist'),
+    publicPath: '/static/',
+    filename: '[name]-bundle.js',
+    chunkFilename: '[name]-[chunkhash].js',
+  },
+  optimization: {
+    removeAvailableModules: false,
+    removeEmptyChunks: false,
+    splitChunks: false,
   },
   resolve: {
     extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+    alias: {
+        '@': path.resolve(__dirname, './src/'),
+        '@api': path.resolve(__dirname, './src/api/'),
+        '@components': path.resolve(__dirname, './src/components/'),
+        '@constants': path.resolve(__dirname, './src/constants/'),
+        '@features': path.resolve(__dirname, './src/features/'),
+        '@hooks': path.resolve(__dirname, './src/hooks/'),
+        '@images': path.resolve(__dirname, './src/images/'),
+        '@styles': path.resolve(__dirname, './src/styles/'),
+        '@utils': path.resolve(__dirname, './src/utils/'),
+    }
+  },
+  devServer: {
+    port: 3000,
+    // host: '0.0.0.0',
+    inline: true,
+    // hot: true,
+    // historyApiFallback: {
+    //  index: '/templates/index/index.html'
+    // },
+    contentBase: path.resolve(__dirname, 'public', 'dist'),
+    historyApiFallback: true,
+    proxy: {
+        '/api': {
+            target: REACT_APP_BASE_URI,
+            ws: true,
+        },
+        '*' : {
+            target: REACT_APP_BASE_URI,
+        }
+    }
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
-        loader: "awesome-typescript-loader",
+        exclude: /node_modules/,
+        use: [
+            {
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                      '@babel/preset-env',
+                      '@babel/preset-typescript',
+                    ],
+                    // plugins: [
+                    //     '@babel/plugin-transform-runtime',
+                    //     '@babel/plugin-transform-arrow-functions',
+                    //     '@babel/plugin-transform-object-assign'
+                    // ]
+                  }
+            },
+            {
+                loader: 'ts-loader',
+                options: {
+                    transpileOnly: true,
+                    experimentalWatchApi: true,
+                },
+            }
+        ]
       },
       {
         enforce: "pre",
@@ -25,17 +93,56 @@ module.exports = {
         loader: "source-map-loader",
       },
       {
-        test: /\.css$/,
-        loader: "css-loader",
-      },
-    ],
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+         MiniCssExtractPlugin.loader,
+        {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+        },
+        {
+            loader: 'resolve-url-loader',
+            options: {
+              debug: true,
+            },
+        },
+        {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+        }
+         //  'css-loader',
+        //  'sass-loader',
+        ],
+       },
+       {
+        test: /\.(png|jpg|jpeg|gif)(\?.*$|$)/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        },
+       },
+       {
+        test: /\.(svg)(\?.*$|$)/,
+        loader: 'url-loader',
+        options: {
+            name: '[name].[ext]',
+        },
+       },
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "public", "index.html"),
     }),
     new MiniCssExtractPlugin({
-      filename: "./src/yourfile.css",
+        filename: "[name].css",
+        chunkFilename: "[id].css"
     }),
+    new ForkTsCheckerPlugin(),
+    new HardSourceWebpackPlugin()
   ],
 };
