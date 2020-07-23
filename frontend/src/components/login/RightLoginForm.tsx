7c025d8e5829142f6901f01b05e3ad0ea1e1a9b7
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-07-23 18:16:40 +0900

    Update RightLoginForm.tsx

--- frontend/src/components/login/RightLoginForm.tsx
+++ frontend/src/components/login/RightLoginForm.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 
-import loginTltMsg from '@images/login/login_tit_msg.png';
+import loginTltMsg from '@/images/login/login_tit_msg.png';
 
 type RightLoginFormProps = {
     setUsername : (name : string) => void,
