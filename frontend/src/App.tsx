eb8261ec0905e166e040f53ae51caf2c1eb01822
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-07-23 18:02:23 +0900

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
 import * as React from "react";
-export interface HelloWorldProps {
-  userName: string;
-  lang: string;
-}
-export const App = (props: HelloWorldProps) => (
-  <h1>
-    Hi {props.userName} from React! Welcome to {props.lang}!
-  </h1>
+import { Switch, Route } from 'react-router-dom';
+import { PublicRoute } from "./routers/PublicRoute";
+import { PrivateRoute } from "./routers/PrivateRoute";
+
+import LoginFeatures from '@features/login'
+import DashboardFeatures from '@/features/admin/dashboard';
+import UserFeatures from '@/features/admin/user';
+
+export const App : React.FC = () => (
+    <Switch>
+        <PublicRoute path="/" restricted={false} component={LoginFeatures}  exact/>
+        <PublicRoute path="/login" restricted={false} component={LoginFeatures} />
+        <PrivateRoute path="/admin/dashboard" component={DashboardFeatures} exact/>
+        <PrivateRoute path="/admin/user" component={UserFeatures} />
+    </Switch>
 );
