c0dafe51f83cbfd49d9d54d072e87b07042ae680
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-07-23 18:08:00 +0900

    Create saga.ts

---
+++ frontend/src/features/login/saga.ts
@@ -0,0 +1,26 @@
+import { call, put, select, takeLatest } from 'redux-saga/effects';
+import { doLogin } from '@api/index';
+import { loginAction, loginSelector } from './slice';
+
+function* handleLogin() {
+    const { loginSuccess, loginFail } = loginAction;
+
+    try {
+
+        const logininfo = yield select(loginSelector.all);
+
+        const res = yield call(doLogin, logininfo.username, logininfo.password);
+
+        yield put(
+            loginSuccess(res.resultData),
+        );
+    } catch (err) {
+        yield put(loginFail(err));
+    }
+}
+
+export function* watchLogin() {
+    const { dologin } = loginAction;
+
+    yield takeLatest(dologin, handleLogin);
+}
