3ce53924ce02ecc68e6dee2373b1600795fa58cd
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-07-23 18:19:45 +0900

    Update index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { App } from "./App";
 import { Helmet } from 'react-helmet';
-import { TITLE } from '@constants/index';
+import { TITLE } from '@/constants/index';
 import './index.scss';
 
 import createStore from './store';
