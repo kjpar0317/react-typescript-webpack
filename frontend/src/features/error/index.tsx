c02705e29374faed202d3695c07a635312eda9fd
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-07-23 18:07:23 +0900

    Create index.tsx

---
+++ frontend/src/features/error/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const ErrorFeatures : React.FC = () => {
+    return (
+        <div>
+            페이지 오류
+        </div>
+    );
+};
+
+export default ErrorFeatures;
