c1a2c74c32f2e8db08cd5550c46173d1541b42ce
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-07-23 18:06:59 +0900

    Create index.tsx

---
+++ frontend/src/features/admin/user/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { withRouter } from "react-router-dom";
+
+const UserFeatures : React.FC = () => {
+    return (
+        <div>
+            이동 성공
+        </div>
+    );
+};
+
+export default withRouter(UserFeatures);
