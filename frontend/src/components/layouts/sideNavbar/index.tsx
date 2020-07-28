363826b47ca03766c661476e2fb80ee572ca6166
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-07-28 09:41:56 +0900

    sideNav 추가

---
+++ frontend/src/components/layouts/sideNavbar/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+
+import SideNavbarItem from './SideNavbarItems';
+
+import loginTit from '@/images/login/login_tit.png';
+
+import '../styles.scss';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+        color: 'white',
+        colorPrimary: 'white'
+    },
+  }),
+);
+
+interface SideNavBarProps {
+    visible: string;
+    menuItems: any;
+}
+
+const SideNavbar : React.FC<SideNavBarProps> = ({visible, menuItems}) => {
+    const classes = useStyles();
+
+    return (
+        <div className={`side-nav ${visible}`}>
+			<div className="logo">
+                <img src={loginTit} alt="CMP" width="170" height="30"/>
+			</div>
+            <List component="nav" className={classes.root} disablePadding>
+                {menuItems.map((item, index) => (
+                    <SideNavbarItem {...item} key={index} id={index} />
+                ))}
+            </List>
+        </div>
+
+    );
+};
+
+export default SideNavbar;
