ff4c75000e6ffc277f5211c97f87e57e5aca1bd7
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-07-28 09:44:41 +0900

    대시보드 수정

--- frontend/src/features/admin/dashboard/index.tsx
+++ frontend/src/features/admin/dashboard/index.tsx
@@ -1,20 +1,28 @@
 import * as React from 'react';
 import { withRouter, useHistory } from "react-router-dom";
 
-import './index.scss';
-
 const DashboardFeatures : React.FC = () => {
-    let history = useHistory();
+    // let history = useHistory();
 
-    const handleMoveTest = () => {
-        history.push('/admin/user');
-    };
+    // const handleMoveTest = () => {
+    //     history.push('/admin/user');
+    // };
 
     return (
-        <div className="dashboard_wrap">
-            로그인 성공
-            <input type="button" value="유저 이동" onClick={handleMoveTest} />
-        </div>
+        <>
+            <div className="widget">
+                <div className="title">Number of views</div>
+                <div className="chart"></div>
+            </div>
+            <div className="widget">
+                <div className="title">Number of likes</div>
+                <div className="chart"></div>
+            </div>
+            <div className="widget">
+                <div className="title">Number of comments</div>
+                <div className="chart"></div>
+            </div>
+        </>
     );
 };
 
