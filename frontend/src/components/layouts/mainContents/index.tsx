60d9a29aa0e695783184fcbc3671e0cbeee83f75
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-07-28 09:41:19 +0900

    메인 컨텐츠 추가

---
+++ frontend/src/components/layouts/mainContents/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { RouteProps } from 'react-router-dom';
+import Typography from '@material-ui/core/Typography';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import Link from '@material-ui/core/Link';
+
+interface MainContentsProps extends RouteProps {
+
+}
+
+const MainContents : React.FC<MainContentsProps> = (props) => {
+    return (
+        <div className="main-content">
+			<div className="title">
+				<Breadcrumbs aria-label="">
+                    <Link color="inherit" href="/">
+                        Dashboard
+                    </Link>
+                    <Link color="inherit" href="/admin/dashboard">
+                        Core
+                    </Link>
+                    <Typography color="textPrimary">Sub</Typography>
+                </Breadcrumbs>
+    		</div>
+            <div className="main">
+                {props.children}
+            </div>
+		</div>
+    );
+};
+
+export default MainContents;
