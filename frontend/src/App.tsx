4c6899d004ffaa71101fbf50c02498bb154c0110
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-07-28 09:37:49 +0900

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import { PublicRoute } from "./routers/PublicRoute";
 import { PrivateRoute } from "./routers/PrivateRoute";
 
@@ -7,11 +7,15 @@ import LoginFeatures from '@/features/login'
 import DashboardFeatures from '@/features/admin/dashboard';
 import UserFeatures from '@/features/admin/user';
 
+import './index.scss';
+
 export const App : React.FC = () => (
+    <>
     <Switch>
         <PublicRoute path="/" restricted={false} component={LoginFeatures}  exact/>
         <PublicRoute path="/login" restricted={false} component={LoginFeatures} />
-        <PrivateRoute path="/admin/dashboard" component={DashboardFeatures} exact/>
+        <PrivateRoute path="/admin/dashboard" component={DashboardFeatures}/>
         <PrivateRoute path="/admin/user" component={UserFeatures} />
     </Switch>
+    </>
 );
