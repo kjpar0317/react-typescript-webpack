f335ec3b489de5779c1498e2269bd7e9991e0611
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-07-28 09:42:26 +0900

    SideNavitem  추가

---
+++ frontend/src/components/layouts/sideNavbar/SideNavbarItems.tsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { makeStyles, createStyles } from '@material-ui/core/styles'
+
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import Divider from '@material-ui/core/Divider'
+import Collapse from '@material-ui/core/Collapse'
+
+import IconExpandLess from '@material-ui/icons/ExpandLess'
+import IconExpandMore from '@material-ui/icons/ExpandMore'
+
+// React runtime PropTypes
+export const AppMenuItemPropTypes = {
+  name: PropTypes.string.isRequired,
+  link: PropTypes.string,
+  Icon: PropTypes.elementType,
+  items: PropTypes.array,
+}
+
+// TypeScript compile-time props type, infered from propTypes
+// https://dev.to/busypeoples/notes-on-typescript-inferring-react-proptypes-1g88
+type AppMenuItemPropTypes = PropTypes.InferProps<typeof AppMenuItemPropTypes>
+type AppMenuItemPropsWithoutItems = Omit<AppMenuItemPropTypes, 'items'>
+
+// Improve child items declaration
+export type AppMenuItemProps = AppMenuItemPropsWithoutItems & {
+    items?: AppMenuItemProps[]
+}
+
+const useStyles = makeStyles(theme => createStyles({
+      menuItem: {},
+      menuItemIcon: {
+        color: '#97c05c',
+      },
+      screen: theme.breakpoints.values
+    }),
+);
+
+const SideNavbarItem: React.FC<AppMenuItemProps> = props => {
+  const { name, Icon, items = [] } = props;
+  const classes = useStyles();
+  const isExpandable = items && items.length > 0;
+  const [open, setOpen] = React.useState(false);
+
+  function handleClick(e : React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    setOpen(!open);
+  }
+
+  const MenuItemRoot = (
+    <ListItem button className={classes.menuItem}
+        onClick={(e) => handleClick(e) }>
+      {/* Display an icon if any */}
+      {!!Icon && (
+        <ListItemIcon className={classes.menuItemIcon}>
+          <Icon />
+        </ListItemIcon>
+      )}
+      <ListItemText primary={name} inset={!Icon}/>
+      {/* Display the expand menu if the item has children */}
+      {isExpandable && !open && <IconExpandMore />}
+      {isExpandable && open && <IconExpandLess />}
+    </ListItem>
+  );
+
+  const MenuItemChildren = isExpandable ? (
+    <Collapse in={open} timeout="auto" unmountOnExit>
+      <Divider />
+      <List component="div" disablePadding>
+        {items.map((item, index) => (
+          <SideNavbarItem {...item} key={index} />
+        ))}
+      </List>
+    </Collapse>
+  ) : null
+
+  return (
+    <>
+      {MenuItemRoot}
+      {MenuItemChildren}
+    </>
+  )
+}
+
+SideNavbarItem.propTypes = AppMenuItemPropTypes
+
+export default SideNavbarItem;
