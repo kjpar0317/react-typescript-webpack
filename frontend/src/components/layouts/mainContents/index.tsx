65d293a401e62692fc8dc4321a43562ff79dbcd8
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-07-30 12:07:17 +0900

    Update index.tsx

--- frontend/src/components/layouts/mainContents/index.tsx
+++ frontend/src/components/layouts/mainContents/index.tsx
@@ -3,30 +3,48 @@ import { RouteProps } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 import Link from '@material-ui/core/Link';
+import withWidth from '@material-ui/core/withWidth';
 
 interface MainContentsProps extends RouteProps {
-
+    sidebarOpen: boolean,
+    width: string,
 }
 
 const MainContents : React.FC<MainContentsProps> = (props) => {
+    const [ paddingLeft, setPaddingLeft ] = React.useState('250px');
+
+    React.useEffect(() => {
+        if(props.width == "xs") {
+            setPaddingLeft('0px');
+        } else if(props.width == "sm") {
+            setPaddingLeft('70px');
+        } else {
+            if(props.sidebarOpen) {
+                setPaddingLeft('250px');
+            } else {
+                setPaddingLeft('70px');
+            }
+        }
+    }, [props]);
+
     return (
-        <div className="main-content">
+        <div className="main-content" style={{paddingLeft: paddingLeft}}>
 			<div className="title">
 				<Breadcrumbs aria-label="">
                     <Link color="inherit" href="/">
-                        Dashboard
+                        HOME
                     </Link>
                     <Link color="inherit" href="/admin/dashboard">
-                        Core
+                        ADMIN
                     </Link>
-                    <Typography color="textPrimary">Sub</Typography>
+                    <Typography color="textPrimary">Dashboard</Typography>
                 </Breadcrumbs>
     		</div>
-            <div className="main">
-                {props.children}
-            </div>
+            {props.children}
 		</div>
     );
 };
 
-export { MainContents };
+const WidthMainContents = withWidth()(MainContents);
+
+export { WidthMainContents as MainContents };
