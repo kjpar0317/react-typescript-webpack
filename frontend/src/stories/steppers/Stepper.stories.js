cff9d1443b0158dd909b9d0354863494d0c8c0c5
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-09-09 18:26:47 +0900

    Create Stepper.stories.js

---
+++ frontend/src/stories/steppers/Stepper.stories.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { IntlProvider } from 'react-intl';
+
+import { storiesOf } from '@storybook/react';
+import { withKnobs, object, number, select } from '@storybook/addon-knobs';
+
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+import createStore from '@/store';
+import { customTheme } from '@/constants';
+import { CStepper } from '@/components/stepper';
+
+import locale_ko from '@/locale/ko.json';
+import locale_en from '@/locale/en.json';
+
+const store = createStore();
+
+const messages = {
+    ko: locale_ko,
+    en: locale_en,
+};
+
+storiesOf('Stepper 컴포넌트', module)
+    .addDecorator((story) => (
+        <IntlProvider locale="ko" messages={messages['ko']}>
+            <Provider store={store}>
+                <Router>
+                    <ThemeProvider theme={createMuiTheme(customTheme)}>
+                        <Route path="/" component={() => story()} />
+                    </ThemeProvider>
+                </Router>
+            </Provider>
+        </IntlProvider>
+    ))
+    .add('VMware Stepper', () => (
+        <CStepper
+            type={select(
+                'stepper 스타일',
+                {
+                    없음: '',
+                    cstepper1: 'cstepper1',
+                    cstepper2: 'cstepper2',
+                    cstepper3: 'cstepper3',
+                    cstepper4: 'cstepper4',
+                },
+                '',
+            )}
+            items={object('stepper items', ['Step1', 'Step2', 'Step3'])}
+            defaultStep={number('Default Step', 0)}
+            orientation={select(
+                '방향',
+                {
+                    horizontal: 'horizontal',
+                    vertical: 'vertical',
+                },
+                'horizontal',
+            )}
+        />
+    ))
+    .addDecorator(withKnobs);
