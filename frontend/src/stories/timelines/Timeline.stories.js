5063e8cc044a58205c885692fc5fa5cae9d182b4
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-09-14 18:19:51 +0900

    Create Timeline.stories.js

---
+++ frontend/src/stories/timelines/Timeline.stories.js
@@ -0,0 +1,137 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { IntlProvider } from 'react-intl';
+
+import { storiesOf } from '@storybook/react';
+import { withKnobs, object, number, select } from '@storybook/addon-knobs';
+
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+import createStore from '@/store';
+import { customTheme } from '@/constants';
+import {
+    CAlternaeTimeline,
+    CLeftTimeline,
+    CRightTimeline,
+} from '@/components/timelines';
+
+import locale_ko from '@/locale/ko.json';
+import locale_en from '@/locale/en.json';
+
+const store = createStore();
+
+const messages = {
+    ko: locale_ko,
+    en: locale_en,
+};
+
+storiesOf('Timeline 컴포넌트', module)
+    .addDecorator((story) => (
+        <IntlProvider locale="ko" messages={messages['ko']}>
+            <Provider store={store}>
+                <Router>
+                    <ThemeProvider theme={createMuiTheme(customTheme)}>
+                        <Route path="/" component={() => story()} />
+                    </ThemeProvider>
+                </Router>
+            </Provider>
+        </IntlProvider>
+    ))
+    .add('Timeline 기본', () => (
+        <CAlternaeTimeline
+            items={object('timeline items', [
+                {
+                    variant: 'outlined',
+                    icon: 'Android',
+                    title: '테스트 제목',
+                    content: '테스트 내용',
+                },
+                {
+                    variant: 'outlined',
+                    title: '테스트 제목2',
+                    content: '테스트 내용2',
+                    revert: {
+                        title: '09:00',
+                        content: '반대 텍스트',
+                    },
+                },
+                {
+                    icon: 'Apple',
+                    type: 'ctimeline2',
+                    variant: 'outlined',
+                    bline: true,
+                    title: '테스트 제목2',
+                    content: '테스트 내용2',
+                },
+                {
+                    type: 'ctimeline1',
+                    icon: 'Done',
+                    content: '테스트 내용4',
+                },
+            ])}
+        />
+    ))
+    .add('Timeline 왼쪽', () => (
+        <CLeftTimeline
+            items={object('timeline items', [
+                {
+                    variant: 'outlined',
+                    icon: 'Android',
+                    title: '테스트 제목',
+                    content: '테스트 내용',
+                },
+                {
+                    variant: 'outlined',
+                    icon: 'Apple',
+                    title: '테스트 제목2',
+                    content: '테스트 내용2',
+                },
+                {
+                    icon: 'Backup',
+                    type: 'ctimeline2',
+                    variant: 'outlined',
+                    bline: true,
+                    title: '테스트 제목2',
+                    content: '테스트 내용2',
+                },
+                {
+                    type: 'ctimeline1',
+                    icon: 'Done',
+                    content: '테스트 내용4',
+                },
+            ])}
+        />
+    ))
+    .add('Timeline 오른쪽', () => (
+        <CRightTimeline
+            items={object('timeline items', [
+                {
+                    variant: 'outlined',
+                    icon: 'Android',
+                    title: '테스트 제목',
+                    content: '테스트 내용',
+                },
+                {
+                    variant: 'outlined',
+                    icon: 'Apple',
+                    title: '테스트 제목2',
+                    content: '테스트 내용2',
+                },
+                {
+                    icon: 'Backup',
+                    type: 'ctimeline2',
+                    variant: 'outlined',
+                    bline: true,
+                    title: '테스트 제목2',
+                    content: '테스트 내용2',
+                },
+                {
+                    type: 'ctimeline1',
+                    icon: 'Done',
+                    content: '테스트 내용4',
+                },
+            ])}
+        />
+    ))
+    .addDecorator(withKnobs);
