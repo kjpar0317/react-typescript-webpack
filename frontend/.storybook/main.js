520fd68a210441b06436048f52603581dbdff862
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-09-17 18:14:52 +0900

    Update main.js

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,27 +1,27 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  stories: ["../src/stories/**/*.stories.(ts|tsx|js|jsx|mdx)"],
-  addons: [
-    "@storybook/addon-actions/register",
-    "@storybook/addon-links/register",
-    '@storybook/addon-knobs/register',
-    '@storybook/addon-notes/register'
-  ],
-  presets: [
-    {
-      name: "@storybook/preset-create-react-app",
-      options: {
-        tsDocgenLoaderOptions: {
-          tsconfigPath: path.resolve(__dirname, "./tsconfig.json")
-        }
-      }
-    },
-    {
-      name: "@storybook/addon-docs/preset",
-      options: {
-        configureJSX: true
-      }
-    }
-  ]
+    stories: ['../src/stories/**/*.stories.(ts|tsx|js|jsx|mdx)'],
+    addons: [
+        '@storybook/addon-actions/register',
+        '@storybook/addon-links/register',
+        '@storybook/addon-knobs',
+        '@storybook/addon-notes/register',
+    ],
+    presets: [
+        {
+            name: '@storybook/preset-create-react-app',
+            options: {
+                tsDocgenLoaderOptions: {
+                    tsconfigPath: path.resolve(__dirname, './tsconfig.json'),
+                },
+            },
+        },
+        {
+            name: '@storybook/addon-docs/preset',
+            options: {
+                configureJSX: true,
+            },
+        },
+    ],
 };
