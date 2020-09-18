1f2b1c7e1b2e66835a4c7315460451da736fa862
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-09-18 09:24:24 +0900

    Update MenuPopItem.tsx

--- frontend/src/components/layouts/sideNavbar/MenuPopItem.tsx
+++ frontend/src/components/layouts/sideNavbar/MenuPopItem.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import { useIntl } from 'react-intl';
+import { join, split } from 'lodash';
 
 import { makeStyles, createStyles } from '@material-ui/core/styles';
-import Menu, { MenuProps } from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
+import Menu, { MenuProps } from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import SubMenuPopItems from './SubMenuPopItems';
 
 interface MenuPopItemsProps extends MenuProps {
@@ -11,25 +13,45 @@ interface MenuPopItemsProps extends MenuProps {
     menuItems: Array<any>;
     open: boolean;
     anchorEl: null | HTMLElement;
-    onClose: (e : any) => void;
+    onClose: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
 }
 
-const useStyles = makeStyles(theme => createStyles({
-    screen: theme.breakpoints.values
-  }),
+const useStyles = makeStyles((theme) =>
+    createStyles({
+        screen: theme.breakpoints.values,
+    }),
 );
 
-const MenuPopItems : React.FC<MenuPopItemsProps> = ({id, open, menuItems, anchorEl, onClose, ...rest}) => {
+const MenuPopItems: React.FC<MenuPopItemsProps> = ({
+    id,
+    open,
+    menuItems,
+    anchorEl,
+    onClose,
+    ...rest
+}) => {
     const classes = useStyles();
+    const { formatMessage } = useIntl();
     let history = useHistory();
 
-    const handleLink = (link : any) => {
+    const handleLink = (e: React.MouseEvent<any, MouseEvent>, link: string) => {
         history.push(link);
+        onClose(e);
+    };
+
+    const convertMenuItemNm = (menuNm: string) => {
+        if (!menuNm.startsWith('w.')) {
+            return menuNm;
+        }
+        return join(
+            split(menuNm, ' ').map((menunm) => formatMessage({ id: menunm })),
+            ' ',
+        );
     };
 
     const renderMenuItems = () => {
-        return menuItems.map(item => {
-            if (item.hasOwnProperty("items")) {
+        return menuItems.map((item) => {
+            if (item.hasOwnProperty('items')) {
                 return (
                     <SubMenuPopItems
                         id={id}
@@ -37,16 +59,20 @@ const MenuPopItems : React.FC<MenuPopItemsProps> = ({id, open, menuItems, anchor
                         caption={item.name}
                         menuItems={item.items}
                         open={true}
+                        onClose={onClose}
                     />
                 );
             }
 
             return (
-                <MenuItem key={item.id} onClick={() => handleLink(item.link)}>
-                    {item.name}
+                <MenuItem
+                    key={item.id}
+                    onClick={(e) => handleLink(e, item.link)}
+                >
+                    {convertMenuItemNm(item.name)}
                 </MenuItem>
             );
-        })
+        });
     };
 
     return (
@@ -57,7 +83,7 @@ const MenuPopItems : React.FC<MenuPopItemsProps> = ({id, open, menuItems, anchor
             open={open}
             onClose={onClose}
         >
-            { renderMenuItems() }
+            {renderMenuItems()}
         </Menu>
     );
 };
