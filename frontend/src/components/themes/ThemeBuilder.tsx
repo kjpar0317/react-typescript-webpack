1c2f1823b5577575f1b322f5423bb452c3a4dace
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-09-28 09:33:46 +0900

    Create ThemeBuilder.tsx

---
+++ frontend/src/components/themes/ThemeBuilder.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import { LeftThemeBuilder } from './LeftThemeBuilder';
+import { RightThemeBuilder } from './RightThemeBuilder';
+
+const ThemeBuilder: React.FC = () => {
+    return (
+        <>
+            <div style={{ width: '20%', float: 'left' }}>
+                <div
+                    style={{
+                        height: '700px',
+                        overflowY: 'auto',
+                        padding: '10px 10px 5px 10px',
+                        backgroundColor: '#eeeeee',
+                    }}
+                >
+                    <LeftThemeBuilder />
+                </div>
+            </div>
+            <div style={{ width: '80%', float: 'left' }}>
+                <div
+                    style={{
+                        width: '100%',
+                        height: '700px',
+                        overflowX: 'hidden',
+                        overflowY: 'auto',
+                        padding: '10px 10px 5px 10px',
+                        backgroundColor: '#eeeeee',
+                    }}
+                >
+                    <RightThemeBuilder />
+                </div>
+            </div>
+        </>
+    );
+};
+
+export { ThemeBuilder };
