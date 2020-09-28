ad30d3bab69d4fe303491f750dbd4ec405497dea
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-09-28 09:32:10 +0900

    Create GridLayoutItem.tsx

---
+++ frontend/src/components/grids/GridLayoutItem.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { GridRenderItem } from '@/components/grids';
+
+interface GridLayoutItemProps {
+    item: any;
+}
+
+const GridLayoutItem: React.FC<GridLayoutItemProps> = ({ item }) => {
+    return (
+        <div
+            className="droptarget"
+            draggable={true}
+            unselectable="on"
+            style={{ width: '100%', height: '100%' }}
+            onDragStart={(e: any) => {
+                e.dataTransfer.dropEffect = 'copy';
+                // text/html
+                e.dataTransfer.setData('text/plain', item.wid);
+            }}
+        >
+            <GridRenderItem item={item} />
+        </div>
+    );
+};
+
+export { GridLayoutItem };
