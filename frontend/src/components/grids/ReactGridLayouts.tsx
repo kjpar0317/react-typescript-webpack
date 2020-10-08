dc7d2bd44361feccccc2d3d8d9ccb9c87007d441
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-10-08 09:19:58 +0900

    Update ReactGridLayouts.tsx

--- frontend/src/components/grids/ReactGridLayouts.tsx
+++ frontend/src/components/grids/ReactGridLayouts.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { pick, cloneDeep } from 'lodash';
 
 import { Responsive, WidthProvider } from 'react-grid-layout';
 
@@ -11,19 +12,49 @@ import 'react-resizable/css/styles.css';
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
 interface GridLayoutProps {
-    layouts: Array<any>;
     items: Array<any>;
     width?: number;
     mounted?: boolean;
     isDroppable?: boolean;
     isEdit?: boolean;
+    nEndLayoutId?: number;
     onLayoutChange?(layouts: Array<any>): void;
 }
 
 const ReactGridLayouts = React.memo<GridLayoutProps>((props) => {
-    const { items, width, mounted, isDroppable, isEdit } = props;
-    const [layouts, setLayouts] = useState<Array<any>>(props.layouts);
-    let [maxConunt, setMaxCount] = useState<number>(props.layouts.length);
+    const {
+        items,
+        width,
+        mounted,
+        isDroppable,
+        isEdit,
+        nEndLayoutId = 0,
+    } = props;
+    const [layouts, setLayouts] = useState<Array<any>>([]);
+    const [maxCount, setMaxCount] = useState<number>(nEndLayoutId + 1);
+
+    useEffect(() => {
+        if (items.length > 0) {
+            let cloneItems = cloneDeep(items);
+
+            let tlayouts = cloneItems.map((item) => {
+                return pick(item, [
+                    'i',
+                    'x',
+                    'y',
+                    'w',
+                    'h',
+                    'minW',
+                    'minH',
+                    'maxW',
+                    'maxH',
+                    'static',
+                ]);
+            });
+
+            setLayouts(tlayouts);
+        }
+    }, [items]);
 
     useEffect(() => {
         // window.dispatchEvent(new Event('resize'));
@@ -34,9 +65,11 @@ const ReactGridLayouts = React.memo<GridLayoutProps>((props) => {
     }, [width]);
 
     const onLayoutChange = (val: any) => {
-        setLayouts(val);
+        // setLayouts(val);
 
-        props.onLayoutChange && props.onLayoutChange(layouts);
+        console.log(val);
+
+        props.onLayoutChange && props.onLayoutChange(val);
     };
 
     const onResize = (val: any) => {
@@ -44,16 +77,24 @@ const ReactGridLayouts = React.memo<GridLayoutProps>((props) => {
     };
 
     const onDrop = (layout: any, layoutItem: any, event: any) => {
-        layout.map((l: any, index: number) => {
-            if (l.i === '__dropping-elem__') {
-                l.i = (maxConunt + 1).toString();
+        // console.log(layout);
+        // console.log(layoutItem);
+        // console.log(event.dataTransfer.getData('text/plain'));
+        // let cloneItems = cloneDeep(layout);
+        let cloneItems = cloneDeep(layout);
+        cloneItems.map((item: any) => {
+            if (item.i === '__dropping-elem__') {
+                item.i =
+                    maxCount.toString() +
+                    ',' +
+                    event.dataTransfer.getData('text/plain');
             }
-
-            // l.i = (index + 1).toString();
         });
 
-        setLayouts(layout);
-        setMaxCount(++maxConunt);
+        console.log(cloneItems);
+
+        setLayouts(cloneItems);
+        setMaxCount(maxCount + 1);
     };
 
     const onDelete = (id: string) => {
@@ -61,58 +102,60 @@ const ReactGridLayouts = React.memo<GridLayoutProps>((props) => {
             return f.i !== id;
         });
 
-        // filteredLayout.map((layout: any, index: number) => {
-        //     layout.i = (index + 1).toString();
-        // });
-
         setLayouts(filteredLayout);
-        // setMaxCount(--maxConunt);
     };
 
     return (
-        <ResponsiveReactGridLayout
-            onResize={onResize}
-            layout={layouts}
-            onLayoutChange={onLayoutChange}
-            onDrop={onDrop}
-            breakpoints={{ lg: 1280, md: 800, sm: 600, xs: 0, xxs: 0 }}
-            cols={{ lg: 12, md: 12, sm: 12, xs: 12, xxs: 12 }}
-            margin={[10, 10]}
-            measureBeforeMount={false}
-            useCSSTransforms={mounted}
-            isDroppable={isDroppable}
-        >
-            {layouts &&
-                layouts.map((layout: any, index: number) => {
-                    const filter: any = items.filter(
-                        (data: any) => data.i === layout.i,
-                    );
+        <div style={{ width: width }}>
+            <ResponsiveReactGridLayout
+                onResize={onResize}
+                layout={layouts}
+                onLayoutChange={onLayoutChange}
+                onDrop={onDrop}
+                breakpoints={{ lg: 1280, md: 800, sm: 600, xs: 0, xxs: 0 }}
+                cols={{ lg: 12, md: 12, sm: 12, xs: 12, xxs: 12 }}
+                marin={[10, 10]}
+                measureBeforeMount={true}
+                mounted={mounted}
+                isBounded={true}
+                isDroppable={isDroppable}
+            >
+                {layouts &&
+                    layouts.map((layout: any, index: number) => {
+                        const filter: any = items.filter(
+                            (data: any) => data.i === layout.i,
+                        );
 
-                    return (
-                        <div className="item" key={layout.i} data-grid={layout}>
-                            {isEdit && (
-                                <div
-                                    style={{
-                                        width: '100%',
-                                        zIndex: 1,
-                                        position: 'inherit',
-                                    }}
-                                >
-                                    <CIconButton
-                                        type="btn4"
-                                        icon="Clear"
-                                        tooltip="Delete"
-                                        size="small"
-                                        style={{ float: 'right' }}
-                                        onClick={() => onDelete(layout.i)}
-                                    />
-                                </div>
-                            )}
-                            <GridRenderItem item={filter[0]} />
-                        </div>
-                    );
-                })}
-        </ResponsiveReactGridLayout>
+                        return (
+                            <div
+                                className="item"
+                                key={layout.i}
+                                data-grid={layout}
+                            >
+                                {isEdit && (
+                                    <div
+                                        style={{
+                                            width: '100%',
+                                            zIndex: 1,
+                                            position: 'inherit',
+                                        }}
+                                    >
+                                        <CIconButton
+                                            type="btn4"
+                                            icon="Clear"
+                                            tooltip="Delete"
+                                            size="small"
+                                            style={{ float: 'right' }}
+                                            onClick={() => onDelete(layout.i)}
+                                        />
+                                    </div>
+                                )}
+                                <GridRenderItem item={filter[0]} />
+                            </div>
+                        );
+                    })}
+            </ResponsiveReactGridLayout>
+        </div>
     );
 });
 
