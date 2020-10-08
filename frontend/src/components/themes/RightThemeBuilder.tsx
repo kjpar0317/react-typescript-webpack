3a19438d255274695bddea1b4d61ff7e569e0a4b
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-10-08 09:21:22 +0900

    Update RightThemeBuilder.tsx

--- frontend/src/components/themes/RightThemeBuilder.tsx
+++ frontend/src/components/themes/RightThemeBuilder.tsx
@@ -1,153 +1,110 @@
 import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+    merge,
+    cloneDeep,
+    clone,
+    filter,
+    isEmpty,
+    includes,
+    sortBy,
+} from 'lodash';
 
+import { dashboardAction, dashboardSelector } from '@/features/dashboard/slice';
 import { ReactGridLayouts } from '@/components/grids';
+import { getLayouts } from '@/api';
 
 export interface TargetBoxProps {}
 
-let layouts = [
-    { i: '1', x: 0, y: 0, w: 3, h: 2, minW: 3, minH: 2, maxH: 6, static: true },
-    { i: '2', x: 3, y: 0, w: 3, h: 2, minW: 3, minH: 2, maxH: 6 },
-    { i: '3', x: 6, y: 0, w: 3, h: 2, minW: 3, minH: 2, maxH: 6 },
-    { i: '4', x: 9, y: 0, w: 3, h: 2, minW: 3, minH: 2, maxH: 6 },
-    { i: '5', x: 0, y: 2, w: 6, h: 2, minW: 3, minH: 2, maxH: 6 },
-    { i: '6', x: 6, y: 2, w: 6, h: 2, minW: 3, minH: 2, maxH: 6 },
-    { i: '7', x: 0, y: 4, w: 12, h: 2, minW: 3, minH: 2, maxH: 6 },
-];
-
-const items = [
-    {
-        i: '1',
-        wid: '1',
-        type: 'text',
-        data: {
-            title: '고정 위젯 테스트',
-            text:
-                'This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.',
-        },
-    },
-    {
-        i: '2',
-        wid: '3',
-        type: 'areachart',
-        xPvt: 'name',
-        yPvts: ['pv', 'amt'],
-        data: [
-            {
-                name: 'Page A',
-                uv: 4000,
-                pv: 2400,
-                amt: 2400,
-            },
-            {
-                name: 'Page B',
-                uv: 3000,
-                pv: 1398,
-                amt: 2210,
-            },
-            {
-                name: 'Page C',
-                uv: 2000,
-                pv: 9800,
-                amt: 2290,
-            },
-            {
-                name: 'Page D',
-                uv: 2780,
-                pv: 3908,
-                amt: 2000,
-            },
-            {
-                name: 'Page E',
-                uv: 1890,
-                pv: 4800,
-                amt: 2181,
-            },
-            {
-                name: 'Page F',
-                uv: 2390,
-                pv: 3800,
-                amt: 2500,
-            },
-            {
-                name: 'Page G',
-                uv: 3490,
-                pv: 4300,
-                amt: 2100,
-            },
-        ],
-    },
-    {
-        i: '3',
-        wid: '1',
-        type: 'text',
-        data: {
-            title: '테스트3',
-            text: '한글 테스트',
-        },
-    },
-    {
-        i: '4',
-        wid: '1',
-        type: 'text',
-        data: {
-            title: '테스트4',
-            text:
-                'This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.',
-        },
-    },
-    {
-        i: '5',
-        wid: '2',
-        type: 'image',
-        data: {
-            title: '테스트5',
-            image:
-                'https://www.ft.com/__origami/service/image/v2/images/raw/https%3A%2F%2Fs3-eu-west-1.amazonaws.com%2Ffta-ez-prod%2Fez%2Fimages%2F1%2F9%2F7%2F1%2F1481791-4-eng-GB%2Farchitecture-camera-city-827209+%281%29.jpg%3Fv1?width=700&source=ftadviser',
-            text:
-                'For advisers, the turbulent economic waters of recent years have been difficult to navigate – Brexit uncertainty, trade wars and monetary policy all continue to add pressure.',
-        },
-    },
-    {
-        i: '6',
-        wid: '2',
-        type: 'image',
-        data: {
-            title: '해가 지는 동안 산들.여름의 아름다운 자연 풍경',
-            image:
-                'https://image.shutterstock.com/image-photo/mountains-during-sunset-beautiful-natural-600w-407021107.jpg',
-            text: '좋네',
-        },
-    },
-    {
-        i: '7',
-        wid: '1',
-        type: 'text',
-        data: { title: '테스트7', text: 'Wow. Failed!!' },
-    },
-];
-
 const RightThemeBuilder: React.FC<TargetBoxProps> = () => {
-    const [rlayout, setRLayout] = useState<any>(layouts);
+    const dispatch = useDispatch();
+    const { activeIndex, publicLayout, privateLayouts } = useSelector(
+        dashboardSelector.all,
+    );
+    const [items, setItems] = useState<any>([]);
+    const [tlayout, setTLayout] = useState<any>([]);
     const [mounted, setMounted] = useState(false);
 
     useEffect(() => {
-        setMounted(true);
+        let layouts: Array<any> = [];
+
+        if (privateLayouts.length > 0) {
+            layouts = privateLayouts[activeIndex];
+        } else {
+            layouts = publicLayout;
+        }
+
+        setItems(layouts);
     }, []);
 
+    useEffect(() => {
+        if (privateLayouts.length > 0) {
+            setItems(privateLayouts[activeIndex]);
+        }
+    }, [privateLayouts]);
+
+    useEffect(() => {
+        if (mounted) {
+            let cloneItems: Array<any> = cloneDeep(items);
+            // let cloneItems: Array<any> = items;
+
+            cloneItems.map((layout: any) => {
+                if (includes(layout.i, ',')) {
+                    const tarr = layout.i.split(',');
+                    layout.i = tarr[0];
+                }
+            });
+
+            dispatch(dashboardAction.setTempLayout(cloneItems));
+            setMounted(false);
+        }
+    }, [mounted]);
+
     const handleLayoutChange = (layouts: Array<any>) => {
-        setRLayout(layouts);
+        let cloneItems: Array<any> = cloneDeep(layouts);
+
+        if (layouts.length > 0) {
+            var bDropped = false;
+            cloneItems.map((layout: any) => {
+                if (includes(layout.i, ',')) {
+                    const tarr = layout.i.split(',');
+                    merge(layout, getLayouts(Number(tarr[1])));
+
+                    setTLayout(layout);
+                } else if (layout.i === '__dropping-elem__') {
+                    // 드롭 중인 것??
+                    bDropped = true;
+                } else {
+                    // 일반, 삭제
+                    merge(layout, filter(items, (f) => f.i === layout.i)[0]);
+                }
+            });
+
+            if (!isEmpty(tlayout)) {
+                cloneItems.concat(tlayout);
+                setTLayout({});
+            }
+
+            if (!bDropped) {
+                setItems(cloneItems);
+                setMounted(true);
+            }
+        } else {
+            dispatch(dashboardAction.setTempLayout([]));
+        }
     };
+
+    const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {};
 
     return (
         // <div ref={drop} style={{ opacity }}>
-        <div className="droptarget">
+        <div className="droptarget" onDrop={handleDrop}>
             <ReactGridLayouts
-                layouts={rlayout}
                 items={items}
                 isDroppable={true}
                 isEdit={true}
-                mounted={mounted}
+                width={950}
+                nEndLayoutId={privateLayouts[activeIndex].length}
                 onLayoutChange={handleLayoutChange}
             />
         </div>
