13962b17eefe2d00fc10910c2c29fd97d64c9b51
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-10-14 09:13:59 +0900

    Update GridRenderItem.tsx

--- frontend/src/components/grids/GridRenderItem.tsx
+++ frontend/src/components/grids/GridRenderItem.tsx
@@ -1,34 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
+import { isEmpty } from 'lodash';
+import axiosUtils from '@/utils/axios-utils';
 
 import { ImageWidget, TextWidget, AreaChartWidget } from '@/components/widgets';
+import {
+    LAYOUT_TYPE_CHART_AREA,
+    LAYOUT_TYPE_IMAGE,
+    LAYOUT_TYPE_TEXT,
+} from '@/constants';
+import { getAreaChartData } from '@/utils/chart-utils';
 
 interface GridRenderItemProps {
     item: any;
 }
 
-const LAYOUT_TYPE_CHART_AREA = 'CA';
-const LAYOUT_TYPE_IMAGE = 'I';
-const LAYOUT_TYPE_TEXT = 'T';
-
 const GridRenderItem: React.FC<GridRenderItemProps> = ({ item }) => {
+    const [ data, setData ] = useState<any>();
+
+    useEffect(() => {
+        if(!isEmpty(item)) {
+            getMethodData();
+        }
+    }, [item]);
+
+    const getMethodData = async() => {
+        let jsonData = item.wgDefault;
+
+        if(!isEmpty(item.jsonUrl)) {
+            if(item.method === 'POST') {
+                jsonData = await axiosUtils.post(item.jsonUrl, { params: item.methodParams });
+            } else {
+                jsonData = await axiosUtils.get(item.jsonUrl, { params: item.methodParams });
+            }
+
+            const chartData = getAreaChartData(jsonData[item.respObjNm]);
+
+            setData(chartData);
+        } else {
+            setData(jsonData);
+        }
+    };
+
     const renderElement = () => {
-        if (item.wgType === LAYOUT_TYPE_IMAGE) {
-            return <ImageWidget title={item.wgTitle} data={item.wgItem} />;
-        } else if (item.wgType === LAYOUT_TYPE_TEXT) {
+        if(isEmpty(item)) { return }
+        if (item.wgGb === LAYOUT_TYPE_IMAGE) {
+            return <ImageWidget title={item.wgTitle} imageUrl={item.pageUrl} text={item.option1} />;
+        } else if (item.wgGb === LAYOUT_TYPE_TEXT) {
             return (
                 <TextWidget
                     title={item.wgTitle}
-                    text={item.wgItem.text}
-                    subtext={item.wgItem.subtext}
+                    text={item.option1}
+                    subtext={item.option2}
                 />
             );
-        } else if (item.wgType === LAYOUT_TYPE_CHART_AREA) {
+        } else if (item.wgGb === LAYOUT_TYPE_CHART_AREA) {
             return (
                 <AreaChartWidget
+                    id={item.i}
                     title={item.wgTitle}
-                    data={item.wgItem.data}
-                    xPvt={item.wgItem.xPvt}
-                    yPvts={item.wgItem.yPvts}
+                    data={data}
+                    xPvt={item.option1}
+                    yPvts={item.option2}
                     height="100%"
                 />
             );
