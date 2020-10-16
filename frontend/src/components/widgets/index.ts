f1a58b6af179b35c60918997243e078582252f0b
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-10-16 09:21:21 +0900

    Update index.ts

--- frontend/src/components/widgets/index.ts
+++ frontend/src/components/widgets/index.ts
@@ -1,3 +1,7 @@
 export * from './TextWidget';
 export * from './ImageWidget';
 export * from './AreaChartWidget';
+export * from './PieChartWidget';
+export * from './ScatterPlotWidget';
+export * from './TopologyChartWidget';
+export * from './GaugeRadialChartWidget';
