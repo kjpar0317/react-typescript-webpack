f2f27bcb30cd4e990d3b016c8b40129e1ae4f4d9
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-10-19 09:17:25 +0900

    Create dummy.ts

---
+++ frontend/src/constants/dummy.ts
@@ -0,0 +1,638 @@
+export const PIE_CHART_DUMMY: Array<object> = [
+    {
+      "key": "Phishing Attack",
+      "data": 10
+    },
+    {
+      "key": "IDS",
+      "data": 14
+    },
+    {
+      "key": "Malware",
+      "data": 5
+    },
+    {
+      "key": "DLP",
+      "data": 18
+    }
+  ];
+
+  export const PIE_ACTIVE_SHAPE_DUMMY = [
+    { name: 'Group A', value: 400 },
+    { name: 'Group B', value: 300 },
+    { name: 'Group C', value: 300 },
+    { name: 'Group D', value: 200 },
+  ];
+
+  export const SCATTER_PLOT_DUMMY: Array<object> = [
+    {
+        id: '100',
+        key: new Date('2019-11-24T08:00:00.000Z'),
+        data: 39
+      },
+      {
+        id: '99',
+        key: new Date('2019-11-25T08:00:00.000Z'),
+        data: 5
+      },
+      {
+        id: '98',
+        key: new Date('2019-11-26T08:00:00.000Z'),
+        data: 12
+      },
+      {
+        id: '97',
+        key: new Date('2019-11-27T08:00:00.000Z'),
+        data: 39
+      },
+      {
+        id: '96',
+        key: new Date('2019-11-28T08:00:00.000Z'),
+        data: 17
+      },
+      {
+        id: '95',
+        key: new Date('2019-11-29T08:00:00.000Z'),
+        data: 22
+      },
+      {
+        id: '94',
+        key: new Date('2019-11-30T08:00:00.000Z'),
+        data: 12
+      },
+      {
+        id: '93',
+        key: new Date('2019-12-01T08:00:00.000Z'),
+        data: 43
+      },
+      {
+        id: '92',
+        key: new Date('2019-12-02T08:00:00.000Z'),
+        data: 40
+      },
+      {
+        id: '91',
+        key: new Date('2019-12-03T08:00:00.000Z'),
+        data: 47
+      },
+      {
+        id: '90',
+        key: new Date('2019-12-04T08:00:00.000Z'),
+        data: 15
+      },
+      {
+        id: '89',
+        key: new Date('2019-12-05T08:00:00.000Z'),
+        data: 31
+      },
+      {
+        id: '88',
+        key: new Date('2019-12-06T08:00:00.000Z'),
+        data: 42
+      },
+      {
+        id: '87',
+        key: new Date('2019-12-07T08:00:00.000Z'),
+        data: 37
+      },
+      {
+        id: '86',
+        key: new Date('2019-12-08T08:00:00.000Z'),
+        data: 4
+      },
+      {
+        id: '85',
+        key: new Date('2019-12-09T08:00:00.000Z'),
+        data: 38
+      },
+      {
+        id: '84',
+        key: new Date('2019-12-10T08:00:00.000Z'),
+        data: 32
+      },
+      {
+        id: '83',
+        key: new Date('2019-12-11T08:00:00.000Z'),
+        data: 27
+      },
+      {
+        id: '82',
+        key: new Date('2019-12-12T08:00:00.000Z'),
+        data: 22
+      },
+      {
+        id: '81',
+        key: new Date('2019-12-13T08:00:00.000Z'),
+        data: 45
+      },
+      {
+        id: '80',
+        key: new Date('2019-12-14T08:00:00.000Z'),
+        data: 42
+      },
+      {
+        id: '79',
+        key: new Date('2019-12-15T08:00:00.000Z'),
+        data: 36
+      },
+      {
+        id: '78',
+        key: new Date('2019-12-16T08:00:00.000Z'),
+        data: 20
+      },
+      {
+        id: '77',
+        key: new Date('2019-12-17T08:00:00.000Z'),
+        data: 34
+      },
+      {
+        id: '76',
+        key: new Date('2019-12-18T08:00:00.000Z'),
+        data: 24
+      },
+      {
+        id: '75',
+        key: new Date('2019-12-19T08:00:00.000Z'),
+        data: 42
+      },
+      {
+        id: '74',
+        key: new Date('2019-12-20T08:00:00.000Z'),
+        data: 23
+      },
+      {
+        id: '73',
+        key: new Date('2019-12-21T08:00:00.000Z'),
+        data: 49
+      },
+      {
+        id: '72',
+        key: new Date('2019-12-22T08:00:00.000Z'),
+        data: 12
+      },
+      {
+        id: '71',
+        key: new Date('2019-12-23T08:00:00.000Z'),
+        data: 29
+      },
+      {
+        id: '70',
+        key: new Date('2019-12-24T08:00:00.000Z'),
+        data: 1
+      },
+      {
+        id: '69',
+        key: new Date('2019-12-25T08:00:00.000Z'),
+        data: 42
+      },
+      {
+        id: '68',
+        key: new Date('2019-12-26T08:00:00.000Z'),
+        data: 24
+      },
+      {
+        id: '67',
+        key: new Date('2019-12-27T08:00:00.000Z'),
+        data: 40
+      },
+      {
+        id: '66',
+        key: new Date('2019-12-28T08:00:00.000Z'),
+        data: 41
+      },
+      {
+        id: '65',
+        key: new Date('2019-12-29T08:00:00.000Z'),
+        data: 39
+      },
+      {
+        id: '64',
+        key: new Date('2019-12-30T08:00:00.000Z'),
+        data: 2
+      },
+      {
+        id: '63',
+        key: new Date('2019-12-31T08:00:00.000Z'),
+        data: 3
+      },
+      {
+        id: '62',
+        key: new Date('2020-01-01T08:00:00.000Z'),
+        data: 23
+      },
+      {
+        id: '61',
+        key: new Date('2020-01-02T08:00:00.000Z'),
+        data: 31
+      },
+      {
+        id: '60',
+        key: new Date('2020-01-03T08:00:00.000Z'),
+        data: 46
+      },
+      {
+        id: '59',
+        key: new Date('2020-01-04T08:00:00.000Z'),
+        data: 16
+      },
+      {
+        id: '58',
+        key: new Date('2020-01-05T08:00:00.000Z'),
+        data: 30
+      },
+      {
+        id: '57',
+        key: new Date('2020-01-06T08:00:00.000Z'),
+        data: 23
+      },
+      {
+        id: '56',
+        key: new Date('2020-01-07T08:00:00.000Z'),
+        data: 18
+      },
+      {
+        id: '55',
+        key: new Date('2020-01-08T08:00:00.000Z'),
+        data: 14
+      },
+      {
+        id: '54',
+        key: new Date('2020-01-09T08:00:00.000Z'),
+        data: 43
+      },
+      {
+        id: '53',
+        key: new Date('2020-01-10T08:00:00.000Z'),
+        data: 4
+      },
+      {
+        id: '52',
+        key: new Date('2020-01-11T08:00:00.000Z'),
+        data: 20
+      },
+      {
+        id: '51',
+        key: new Date('2020-01-12T08:00:00.000Z'),
+        data: 23
+      },
+      {
+        id: '50',
+        key: new Date('2020-01-13T08:00:00.000Z'),
+        data: 25
+      },
+      {
+        id: '49',
+        key: new Date('2020-01-14T08:00:00.000Z'),
+        data: 24
+      },
+      {
+        id: '48',
+        key: new Date('2020-01-15T08:00:00.000Z'),
+        data: 6
+      },
+      {
+        id: '47',
+        key: new Date('2020-01-16T08:00:00.000Z'),
+        data: 41
+      },
+      {
+        id: '46',
+        key: new Date('2020-01-17T08:00:00.000Z'),
+        data: 10
+      },
+      {
+        id: '45',
+        key: new Date('2020-01-18T08:00:00.000Z'),
+        data: 39
+      },
+      {
+        id: '44',
+        key: new Date('2020-01-19T08:00:00.000Z'),
+        data: 48
+      },
+      {
+        id: '43',
+        key: new Date('2020-01-20T08:00:00.000Z'),
+        data: 45
+      },
+      {
+        id: '42',
+        key: new Date('2020-01-21T08:00:00.000Z'),
+        data: 29
+      },
+      {
+        id: '41',
+        key: new Date('2020-01-22T08:00:00.000Z'),
+        data: 18
+      },
+      {
+        id: '40',
+        key: new Date('2020-01-23T08:00:00.000Z'),
+        data: 18
+      },
+      {
+        id: '39',
+        key: new Date('2020-01-24T08:00:00.000Z'),
+        data: 23
+      },
+      {
+        id: '38',
+        key: new Date('2020-01-25T08:00:00.000Z'),
+        data: 46
+      },
+      {
+        id: '37',
+        key: new Date('2020-01-26T08:00:00.000Z'),
+        data: 36
+      },
+      {
+        id: '36',
+        key: new Date('2020-01-27T08:00:00.000Z'),
+        data: 1
+      },
+      {
+        id: '35',
+        key: new Date('2020-01-28T08:00:00.000Z'),
+        data: 7
+      },
+      {
+        id: '34',
+        key: new Date('2020-01-29T08:00:00.000Z'),
+        data: 5
+      },
+      {
+        id: '33',
+        key: new Date('2020-01-30T08:00:00.000Z'),
+        data: 41
+      },
+      {
+        id: '32',
+        key: new Date('2020-01-31T08:00:00.000Z'),
+        data: 29
+      },
+      {
+        id: '31',
+        key: new Date('2020-02-01T08:00:00.000Z'),
+        data: 41
+      },
+      {
+        id: '30',
+        key: new Date('2020-02-02T08:00:00.000Z'),
+        data: 11
+      },
+      {
+        id: '29',
+        key: new Date('2020-02-03T08:00:00.000Z'),
+        data: 33
+      },
+      {
+        id: '28',
+        key: new Date('2020-02-04T08:00:00.000Z'),
+        data: 35
+      },
+      {
+        id: '27',
+        key: new Date('2020-02-05T08:00:00.000Z'),
+        data: 38
+      },
+      {
+        id: '26',
+        key: new Date('2020-02-06T08:00:00.000Z'),
+        data: 49
+      },
+      {
+        id: '25',
+        key: new Date('2020-02-07T08:00:00.000Z'),
+        data: 22
+      },
+      {
+        id: '24',
+        key: new Date('2020-02-08T08:00:00.000Z'),
+        data: 38
+      },
+      {
+        id: '23',
+        key: new Date('2020-02-09T08:00:00.000Z'),
+        data: 6
+      },
+      {
+        id: '22',
+        key: new Date('2020-02-10T08:00:00.000Z'),
+        data: 15
+      },
+      {
+        id: '21',
+        key: new Date('2020-02-11T08:00:00.000Z'),
+        data: 37
+      },
+      {
+        id: '20',
+        key: new Date('2020-02-12T08:00:00.000Z'),
+        data: 18
+      },
+      {
+        id: '19',
+        key: new Date('2020-02-13T08:00:00.000Z'),
+        data: 20
+      },
+      {
+        id: '18',
+        key: new Date('2020-02-14T08:00:00.000Z'),
+        data: 38
+      },
+      {
+        id: '17',
+        key: new Date('2020-02-15T08:00:00.000Z'),
+        data: 48
+      },
+      {
+        id: '16',
+        key: new Date('2020-02-16T08:00:00.000Z'),
+        data: 31
+      },
+      {
+        id: '15',
+        key: new Date('2020-02-17T08:00:00.000Z'),
+        data: 20
+      },
+      {
+        id: '14',
+        key: new Date('2020-02-18T08:00:00.000Z'),
+        data: 7
+      },
+      {
+        id: '13',
+        key: new Date('2020-02-19T08:00:00.000Z'),
+        data: 48
+      },
+      {
+        id: '12',
+        key: new Date('2020-02-20T08:00:00.000Z'),
+        data: 35
+      },
+      {
+        id: '11',
+        key: new Date('2020-02-21T08:00:00.000Z'),
+        data: 9
+      },
+      {
+        id: '10',
+        key: new Date('2020-02-22T08:00:00.000Z'),
+        data: 3
+      },
+      {
+        id: '9',
+        key: new Date('2020-02-23T08:00:00.000Z'),
+        data: 10
+      },
+      {
+        id: '8',
+        key: new Date('2020-02-24T08:00:00.000Z'),
+        data: 25
+      },
+      {
+        id: '7',
+        key: new Date('2020-02-25T08:00:00.000Z'),
+        data: 31
+      },
+      {
+        id: '6',
+        key: new Date('2020-02-26T08:00:00.000Z'),
+        data: 39
+      },
+      {
+        id: '5',
+        key: new Date('2020-02-27T08:00:00.000Z'),
+        data: 19
+      },
+      {
+        id: '4',
+        key: new Date('2020-02-28T08:00:00.000Z'),
+        data: 17
+      },
+      {
+        id: '3',
+        key: new Date('2020-02-29T08:00:00.000Z'),
+        data: 25
+      },
+      {
+        id: '2',
+        key: new Date('2020-03-01T08:00:00.000Z'),
+        data: 45
+      },
+      {
+        id: '1',
+        key: new Date('2020-03-02T08:00:00.000Z'),
+        data: 25
+      }
+  ];
+
+export const TOPOLOGY_DATA_DUMMY = {
+    nodes: [
+        { id: 1, label: 'Server', shape: 'image', imageGb: 'server' },
+        { id: 2, label: 'Storage', shape: 'image', imageGb: 'storage' },
+        { id: 3, label: 'Backup', shape: 'image', imageGb: 'backup' },
+        { id: 4, label: 'Software', shape: 'image', imageGb: 'software' },
+        { id: 5, label: 'Database', shape: 'image', imageGb: 'database' }
+    ],
+    edges: [
+        { from: 1, to: 2 },
+        { from: 1, to: 3 },
+        { from: 2, to: 4 },
+        { from: 2, to: 5 }
+    ]
+};
+
+export const RADIAL_DATA_DUMMY = [
+    {
+      "key": "Phishing Attack",
+      "data": 10
+    },
+    {
+      "key": "IDS",
+      "data": 14
+    },
+    {
+      "key": "Malware",
+      "data": 5
+    },
+    {
+      "key": "DLP",
+      "data": 18
+    }
+];
+
+export const BOARD_DATA_DUMMY = [
+    {
+        "title": "테스트 입니다.1",
+        "count": "1",
+        "date": "2020-01-01"
+    },
+    {
+        "title": "테스트 입니다.2",
+        "count": "2",
+        "date": "2020-01-02"
+    },
+    {
+        "title": "테스트 입니다.3",
+        "count": "3",
+        "date": "2020-01-03"
+    },
+    {
+        "title": "테스트 입니다.4",
+        "count": "4",
+        "date": "2020-01-04"
+    },
+    {
+        "title": "테스트 입니다.5",
+        "count": "5",
+        "date": "2020-01-05"
+    },
+    {
+        "title": "테스트 입니다.6",
+        "count": "6",
+        "date": "2020-01-06"
+    },
+    {
+        "title": "테스트 입니다.7",
+        "count": "7",
+        "date": "2020-01-07"
+    },
+];
+
+export const TIMELINE_DATA_DUMMY = [
+    {
+        variant: 'outlined',
+        type: 'ctimeline2',
+        icon: 'Android',
+        content: '정상 시작',
+        revert: {
+            title: '08:00',
+        },
+    },
+    {
+        variant: 'outlined',
+        content: '장애 예측',
+        revert: {
+            title: '09:00',
+        },
+    },
+    {
+        icon: 'Apple',
+        type: 'ctimeline4',
+        variant: 'outlined',
+        content: '장애 발생',
+        revert: {
+            title: '09:30',
+        },
+    },
+    {
+        icon: 'Done',
+        type: 'ctimeline1',
+        variant: 'outlined',
+        content: '정상 복구되었습니다.',
+        revert: {
+            title: '12:00',
+        },
+    },
+];
