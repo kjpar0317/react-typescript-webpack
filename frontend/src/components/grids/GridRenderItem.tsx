b5e63bce1ea75ebe2c8617b8221189b4fa4f9269
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-10-19 09:14:42 +0900

    Update GridRenderItem.tsx

--- frontend/src/components/grids/GridRenderItem.tsx
+++ frontend/src/components/grids/GridRenderItem.tsx
@@ -2,8 +2,9 @@ import React, { useState, useEffect }  from 'react';
 import { isEmpty, trim } from 'lodash';
 import axiosUtils from '@/utils/axios-utils';
 
-import { ImageWidget, TextWidget, AreaChartWidget, PieChartWidget, ScatterPlotWidget, TopologyChartWidget, GaugeRadialChartWidget } from '@/components/widgets';
+import { BoardWidget, ImageWidget, TextWidget, AreaChartWidget, PieChartWidget, ScatterPlotWidget, TopologyChartWidget, GaugeRadialChartWidget, TimelineWidget } from '@/components/widgets';
 import {
+    LAYOUT_TYPE_BOARD,
     LAYOUT_TYPE_CHART_AREA,
     LAYOUT_TYPE_CHART_PIE,
     LAYOUT_TYPE_CHART_PIE_ACTIVESHAPE,
@@ -12,12 +13,14 @@ import {
     LAYOUT_TYPE_CHART_RADIAL,
     LAYOUT_TYPE_IMAGE,
     LAYOUT_TYPE_TEXT,
+    LAYOUT_TYPE_O_TIMELINE,
+    BOARD_DATA_DUMMY,
     PIE_CHART_DUMMY,
     PIE_ACTIVE_SHAPE_DUMMY,
     SCATTER_PLOT_DUMMY,
     TOPOLOGY_DATA_DUMMY,
     RADIAL_DATA_DUMMY,
-
+    TIMELINE_DATA_DUMMY
 } from '@/constants';
 import { getAreaChartData } from '@/utils/chart-utils';
 import { PieActiveShapeChartWidget } from '../widgets/PieActiveShapeChartWidget';
@@ -26,9 +29,14 @@ interface GridRenderItemProps {
     item: any;
 }
 
+const GRID_BOARD_PER_PAGE_NUM = 7;
+
 const GridRenderItem: React.FC<GridRenderItemProps> = ({ item }) => {
     const [ data, setData ] = useState<Array<any>>([]);
     const [ tplyData, setTpLyData ] = useState<any>();
+    const [ totNum, setTotNum ] = useState<number>(0);
+    const [ rowPerPage, setRowPerPage ] = useState<number>(1);
+    const [ page, setPage ] = useState<number>(1);
 
     useEffect(() => {
         if(!isEmpty(item)) {
@@ -42,6 +50,13 @@ const GridRenderItem: React.FC<GridRenderItemProps> = ({ item }) => {
         if(!isEmpty(item.jsonUrl)) {
             if(item.method === 'POST') {
                 switch(item.wgGb) {
+                    case LAYOUT_TYPE_BOARD:
+                        // rowPerPage -> item.h * GRID_BOARD_PER_PAGE_NUM
+                        setPage(1);
+                        setRowPerPage(item.h * GRID_BOARD_PER_PAGE_NUM);
+                        setTotNum(BOARD_DATA_DUMMY.length);
+                        setData(BOARD_DATA_DUMMY);
+                        break;
                     case LAYOUT_TYPE_CHART_AREA:
                         axiosUtils.post(item.jsonUrl, { params: item.methodParams }).then((res: any) => {
                             setData(getAreaChartData(res[item.respObjNm]));
@@ -62,9 +77,18 @@ const GridRenderItem: React.FC<GridRenderItemProps> = ({ item }) => {
                     case LAYOUT_TYPE_CHART_RADIAL:
                         setData(RADIAL_DATA_DUMMY);
                         break;
+                    case LAYOUT_TYPE_O_TIMELINE:
+                        setData(TIMELINE_DATA_DUMMY);
+                        break;
                 };
             } else {
                 switch(item.wgGb) {
+                    case LAYOUT_TYPE_BOARD:
+                        setPage(1);
+                        setRowPerPage(item.h * GRID_BOARD_PER_PAGE_NUM);
+                        setTotNum(BOARD_DATA_DUMMY.length);
+                        setData(BOARD_DATA_DUMMY);
+                        break;
                     case LAYOUT_TYPE_CHART_AREA:
                         axiosUtils.get(item.jsonUrl, { params: item.methodParams }).then((res: any) => {
                             setData(getAreaChartData(res[item.respObjNm]));
@@ -85,16 +109,29 @@ const GridRenderItem: React.FC<GridRenderItemProps> = ({ item }) => {
                     case LAYOUT_TYPE_CHART_RADIAL:
                         setData(RADIAL_DATA_DUMMY);
                         break;
+                    case LAYOUT_TYPE_O_TIMELINE:
+                        setData(TIMELINE_DATA_DUMMY);
+                        break;
                 };
             }
         } else {
-            setData(jsonData);
+            if(item.wgGb === LAYOUT_TYPE_CHART_TOPOLOGY) {
+                setTpLyData(jsonData);
+            } else {
+                setData(jsonData);
+            }
         }
+    };
+
+    const handlePage = (page: number) => {
+        setPage(page);
     };
 
     const renderElement = () => {
         if(isEmpty(item)) { return }
-        if (item.wgGb === LAYOUT_TYPE_IMAGE) {
+        if (item.wgGb === LAYOUT_TYPE_BOARD) {
+            return <BoardWidget title={item.wgTitle} total={totNum} data={data} rowPerPage={rowPerPage} onChange={handlePage} />;
+        } else if (item.wgGb === LAYOUT_TYPE_IMAGE) {
             return <ImageWidget title={item.wgTitle} imageUrl={item.pageUrl} text={item.option1} />;
         } else if (item.wgGb === LAYOUT_TYPE_TEXT) {
             return (
@@ -138,6 +175,10 @@ const GridRenderItem: React.FC<GridRenderItemProps> = ({ item }) => {
         } else if (item.wgGb === LAYOUT_TYPE_CHART_RADIAL) {
             return (
                 <GaugeRadialChartWidget title={item.wgTitle} data={data} />
+            );
+        } else if(item.wgGb === LAYOUT_TYPE_O_TIMELINE) {
+            return (
+                <TimelineWidget title={item.wgTitle} data={data} />
             );
         } else {
             console.log('not supported item type');
