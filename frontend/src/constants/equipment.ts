3be59bd160d6dbec89b5187b875f93a596395804
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-10-19 09:17:40 +0900

    Create equipment.ts

---
+++ frontend/src/constants/equipment.ts
@@ -0,0 +1,10 @@
+// EQUIP_SEVER, EQUIP_STORAGE, EQUIP_SECURITY, EQUIP_BACKUP, EQUIP_NETWORK, EQUIP_CIRCUIT, EQUIP_SOFTWARE, EQUIP_DATABASE
+export const EQUIP_SEVER = 'server';
+export const EQUIP_STORAGE = 'storage';
+export const EQUIP_SECURITY = 'security';
+export const EQUIP_BACKUP = 'backup';
+export const EQUIP_NETWORK = 'network';
+export const EQUIP_CIRCUIT = 'circuit';
+export const EQUIP_SOFTWARE = 'software';
+export const EQUIP_DATABASE = 'database';
+
