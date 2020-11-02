4749a6698e5f08a64951148dfca622a6c4419e5a
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-11-02 09:18:36 +0900

    Update BeanUtils.java

--- backend/test/BeanUtils.java
+++ backend/test/BeanUtils.java
@@ -1,10 +1,32 @@
 package com.innogrid.tabcloudit.batch.common;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
 
-public class BeanUtils {
-    public static Object getBean(String beanName) {
-        ApplicationContext applicationContext = ApplicationContextProvider.getApplicationContext();
-        return applicationContext.getBean(beanName);
-    }
+public class BatchUtils {
+	public static DefaultTransactionAttribute getDefaultTransactionAttribute() {
+		DefaultTransactionAttribute attribute = new DefaultTransactionAttribute();
+		
+		attribute.setPropagationBehavior(Propagation.REQUIRED.value()); //
+		attribute.setIsolationLevel(Isolation.DEFAULT.value());	// https://effectivesquid.tistory.com/entry/%EB%8D%B0%EC%9D%B4%ED%84%B0%EB%B2%A0%EC%9D%B4%EC%8A%A4-Isolation-Level
+		attribute.setTimeout(30);
+
+		return attribute;
+	}
+	
+	public static TaskExecutor getDefaultTaskExecutor(int poolSize) {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor(); // (2)
+        
+		executor.setCorePoolSize(poolSize);
+        executor.setMaxPoolSize(poolSize);
+        executor.setThreadNamePrefix("multi-thread-");
+        executor.setWaitForTasksToCompleteOnShutdown(Boolean.TRUE);
+        executor.setAwaitTerminationSeconds(30);
+        executor.initialize();
+        
+        return executor;
+	}
 }
