44b728b5d5cc00348dce6c12ab65ed5b809a1619
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-11-02 09:23:32 +0900

    Create JobCompleteListener.java

---
+++ backend/test/JobCompleteListener.java
@@ -0,0 +1,19 @@
+package com.innogrid.tabcloudit.batch.job.listener;
+
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.listener.JobExecutionListenerSupport;
+import org.springframework.stereotype.Component;
+
+@JobScope
+@Component
+public class JobCompleteListener extends JobExecutionListenerSupport {
+	@Override
+	public void afterJob(JobExecution jobExecution) {
+		if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
+			System.out.println("BATCH JOB COMPLETED SUCCESSFULLY");
+		}
+	}
+
+}
